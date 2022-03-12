fe828ac774e8fdd00f86fc56ca65f7573108e906
Przemysław Kleist <pk0202@protonmail.com>
2022-03-12 17:21:49 +0100

    find_packages is not working correctly, will be fix later

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     author='kleist0202',
     author_email='pk0202@protonmail.com',
     url='https://github.com/kleist0202/nepygui',
-    packages=find_packages(exclude=('tests*')),
+    # packages=find_packages(exclude=('tests*')),
+    packages=['nepygui'],
 )

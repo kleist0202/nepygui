b3e83f0ef18beb7d2776c6feacf9bede0b600e70
Przemysław Kleist <pk0202@protonmail.com>
2022-03-12 16:17:44 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages 
+
+setup(
+    name="nepygui",
+    version="0.1",
+    description='non-extraordinary python gui package',
+    author='kleist0202',
+    author_email='pk0202@protonmail.com',
+    url='https://github.com/kleist0202/nepygui',
+    packages=find_packages(exclude=('tests*')),
+)

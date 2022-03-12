4bad35e3ee287eeaf0a2c34f343e056d0d589f94
Przemysław Kleist <pk0202@protonmail.com>
2022-03-12 16:47:31 +0100

    change import name

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,36 +1,36 @@
 import pygame
 import argparse
-from context import gui
+from context import nepygui
 
 p = argparse.ArgumentParser(formatter_class=argparse.MetavarTypeHelpFormatter)
 
 def main(args):
-    w = gui.Window(**vars(args))
-    f = gui.TextFrame(fontsize=12,text="ddddddddd", x=200, w=100, fill=gui.Colors.Black, gradient=True, gradientstart=(0,0,255), gradientend=(0,255,0), hover=True, hovercolor=gui.Colors.Red)
-    b = gui.Button(x=200, y=200, w=100, h=100, text="yyyyy", hover=True, gradient=True, fill=(200,10,1))
-    e = gui.EntryWidget(x=400, y=400, w=200, h=60, borderthickness=6)
-    e2 = gui.EntryWidget(x=400, y=200, w=200, h=60)
+    w = nepygui.Window(**vars(args))
+    f = nepygui.TextFrame(fontsize=12,text="ddddddddd", x=200, w=100, fill=nepygui.Colors.Black, gradient=True, gradientstart=(0,0,255), gradientend=(0,255,0), hover=True, hovercolor=nepygui.Colors.Red)
+    b = nepygui.Button(x=200, y=200, w=100, h=100, text="yyyyy", hover=True, gradient=True, fill=(200,10,1))
+    e = nepygui.EntryWidget(x=400, y=400, w=200, h=60, borderthickness=6)
+    e2 = nepygui.EntryWidget(x=400, y=200, w=200, h=60)
 
-    vlayout = gui.VLayout(w, orientation="C")
-    hlayout = gui.HLayout(w, orientation="C")
-    gridlayout = gui.GridLayout(w, orientation="W")
+    vlayout = nepygui.VLayout(w, orientation="C")
+    hlayout = nepygui.HLayout(w, orientation="C")
+    gridlayout = nepygui.GridLayout(w, orientation="W")
 
     vlayout.add_widget(e2)
     vlayout.add_widget(e)
 
-    gridlayout.add_widget(gui.Button(), 4, 4)
-    gridlayout.add_widget(gui.Button(), 4, 0)
-    gridlayout.add_widget(gui.Button(), 4, 0)
-    gridlayout.add_widget(gui.Button(), 4, 2)
-    gridlayout.add_widget(gui.Button(), 0, 4)
-    gridlayout.add_widget(gui.Button(), 2, 4)
-    gridlayout.add_widget(gui.Button(), 4, 4)
-    gridlayout.add_widget(gui.Button(), 5, 1)
+    gridlayout.add_widget(nepygui.Button(), 4, 4)
+    gridlayout.add_widget(nepygui.Button(), 4, 0)
+    gridlayout.add_widget(nepygui.Button(), 4, 0)
+    gridlayout.add_widget(nepygui.Button(), 4, 2)
+    gridlayout.add_widget(nepygui.Button(), 0, 4)
+    gridlayout.add_widget(nepygui.Button(), 2, 4)
+    gridlayout.add_widget(nepygui.Button(), 4, 4)
+    gridlayout.add_widget(nepygui.Button(), 5, 1)
 
     val = 0
     for i in range(4):
         for j in range(4):
-            bij = gui.Button(w=50, h=50, text=str(val), hover=True, gradient=True, fill=(200,10,1), gradientstart=(255,255,0), gradientend=(0,50,50))
+            bij = nepygui.Button(w=50, h=50, text=str(val), hover=True, gradient=True, fill=(200,10,1), gradientstart=(255,255,0), gradientend=(0,50,50))
             gridlayout.add_widget(bij, i, j)
             val += 1
 
